01e9aa7bbcaf4c0bb587d187d0af9d19aa0997cb
Paul <pjensen12@nevada.unr.edu>
2024-09-21 11:45:57 -0600

    Fixed CONFs

--- components/simplepid/__init__.py
+++ components/simplepid/__init__.py
@@ -21,6 +21,6 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    
-    cg.add(var.set_kp(params[CONF_KP]))
-    cg.add(var.set_ki(params[CONF_KI]))
+
+    cg.add(var.set_kp(params[CONF_P]))
+    cg.add(var.set_ki(params[CONF_I]))
