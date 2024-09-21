b6d12b6791b573546ebc5ccf181773c5ec672b62
Paul <pjensen12@nevada.unr.edu>
2024-09-21 14:16:20 -0600

    Try#7

--- components/simplepid/__init__.py
+++ components/simplepid/__init__.py
@@ -22,5 +22,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_p(CONF_P))
-    cg.add(var.set_i(CONF_I))
+    cg.add(var.set_p(config[CONF_P]))
+    cg.add(var.set_i(config[CONF_I]))
