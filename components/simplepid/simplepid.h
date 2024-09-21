015c858b47ae9217fe2bb41ea61a8f81bb41bfd8
Paul <pjensen12@nevada.unr.edu>
2024-09-21 15:22:40 -0600

    Updated Init

--- components/simplepid/simplepid.h
+++ components/simplepid/simplepid.h
@@ -9,22 +9,35 @@ class SimplePID : public Component  {
 
  public:
 
-  // Main Functions
+  // Base Functions. From ESPHome templates
   void setup() override;
   void loop() override;
   void dump_config() override;
 
+  // Calculation Functions
+  //void error_calc();
+
   // Set Parameters Functions
   void set_p(float proportional) {(*this).p_=proportional;}
   void set_i(float integral) {(*this).i_=integral;}
+  void set_d(float derviative) {(*this).d_= derviative;}
+  void set_bias(float bias) {(*this).bias_ = bias;}
 
   // Get Parameters Functions
   float get_p() {return (*this).p_;}
   float get_i() {return (*this).i_;}
+  float get_bias() {return (*this).bias_;}
 
  private:
-  float p_ = 0;
-  float i_ = 0;
+  // Input variables
+  float p_ = 0.0; //unit value where the output will be 100%
+  float i_ = 0.0; //integral rate per Minute
+  float d_ = 0.0; //NOT USED
+  float bias_ = 0.0;
+
+  // Output Values
+  float output = 0.0;
+  
   
 };
 
