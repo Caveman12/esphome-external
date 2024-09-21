9851f744074f839a746c7a3da63fff8fc5acf1ac
Paul <pjensen12@nevada.unr.edu>
2024-09-21 08:30:47 -0600

    adjustments

--- components/simple_pid/__init__.py
+++ components/simple_pid/__init__.py
@@ -8,12 +8,12 @@ SimplePID = simple_pid_ns.class_("SimplePID", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(SimplePID)
+        cv.GenerateID(): cv.declare_id(SimplePID),
         cv.Optional(CONF_NAME)
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID, CONF_NAME])
     await cg.register_component(var, config)
