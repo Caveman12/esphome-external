a3174d817ab65421a10df523447091798607c8a6
Paul <pjensen12@nevada.unr.edu>
2024-09-21 14:15:05 -0600

    Try#6

--- components/simplepid/simplepid.h
+++ components/simplepid/simplepid.h
@@ -15,8 +15,8 @@ class SimplePID : public Component  {
   void dump_config() override;
 
   // Set Parameters Functions
-  void set_p(float p) {SimplePID.p_ = proportional;}
-  void set_i(float i) {SimplePID.i_ = integral;}
+  void set_p(float proportional) {SimplePID.p_ = proportional;}
+  void set_i(float integral) {SimplePID.i_ = integral;}
 
  private:
   float p_ = 0;
