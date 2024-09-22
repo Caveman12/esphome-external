c0a2e177610b613160743df6b6598a61b7bcbcef
Paul <pjensen12@nevada.unr.edu>
2024-09-22 12:04:11 -0600

    Updated init

--- components/simplepid/__init__.py
+++ components/simplepid/__init__.py
@@ -28,6 +28,9 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
+    sens = await cg.get_variable(config[CONF_CONTROL_VARIABLE])
+    cg.add(var.set_sensor(sens))
+    
     cg.add(var.set_p(config[CONF_P]))
     cg.add(var.set_i(config[CONF_I]))
     cg.add(var.set_bias(config[CONF_BIAS]))
